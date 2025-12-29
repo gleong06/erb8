8d226a4b8aff340c2443328c9fa41a3e719406e0
gleong06 <gleong06@yahoo.com.hk>
2025-12-29 17:13:06 +0800

    Step 6 : index page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from listings.models import Listing
 
 # Create your views here.
 def index(request):
-    print(request, request.path)
-    return render(request,'pages/index.html')
+    # print(request, request.path)
+    listings = Listing.objects.all()
+    context = {'listings': listings}
+    return render(request,'pages/index.html', context)
 
 def about(request):
     print(request, request.path)
